4e81f0c902fc11be4ec7dadb78853d890e83f2fc
Kyle Diedrick <corporealshift@gmail.com>
2026-04-18 15:33:06 -0400

    lopress-core: HTML-comment block delimiter scanner

--- crates/lopress-core/src/lib.rs
+++ crates/lopress-core/src/lib.rs
@@ -1,6 +1,8 @@
+pub mod delimiter;
 pub mod error;
 pub mod frontmatter;
 pub mod types;
 
+pub use delimiter::{scan as scan_delimiters, Delim};
 pub use error::ParseError;
 pub use types::{Block, Document, FrontMatter};
